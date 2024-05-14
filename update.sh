90cf01141581b85d741a63e269bc37e1d57ffa86
Mr.KLeo <59167582+lizhuang-zhi@users.noreply.github.com>
2024-05-14 13:09:54 +0800

    update

--- update.sh
+++ update.sh
@@ -16,9 +16,7 @@ HOST=10.1.27.86  # 执行部署的服务器地址
 ######### 部署server1 ############
 echo "Update server1 project..."
 
-pwd
-
-ls -la
+cd .. # 回到根目录
 
 # 设置nginx权重（server1无权重）
 scp -i /var/lib/jenkins/.ssh/id_rsa nginx/server.1.conf root@${HOST}:/etc/nginx/conf.d/server.conf || { echo "scp server.1.conf failed"; exit 1; }
